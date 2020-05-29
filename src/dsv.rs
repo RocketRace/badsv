4dfd6420c7db08a3d1f71b0aaa7858a27d12a5b4
Olivia Palmu <oliviaspalmu@gmail.com>
2020-05-29 15:20:32 +0900

    Parse CSV

--- src/dsv.rs
+++ src/dsv.rs
@@ -1,2 +1,18 @@
-// Parse DSV file (given delimiter), return vec vec
-// Write data to DSV file (given delimiter and line break style, as well as encoding)
+use csv;
+
+use std::fs::File;
+
+/// Parses the contents of a DSV file, and returns 
+/// the grid of items given the delimiter used.
+pub fn parse(data: File, delimiter: u8) -> Vec<Vec<String>> {
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(delimiter)
+        .from_reader(data);
+    let out = reader
+        .records()
+        .map(
+            |r| r.unwrap().iter().map(|s| String::from(s)).collect()
+        )
+        .collect::<Vec<Vec<String>>>();
+    out
+}
