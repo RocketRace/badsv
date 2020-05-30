cf6b71c2e1cc2fa8409535b8959c173fd02d8987
Olivia Palmu <oliviaspalmu@gmail.com>
2020-05-30 12:28:02 +0900

    Build around the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod dsv;
 mod encodings;
 
+use std::fs::File;
 use clap::{App, Arg, ArgGroup, crate_version, crate_authors};
+
+const VALID_ENCODINGS: [&str; 2] = ["utf-8", "utf-16"];
 
 fn main() {
     // Command line arguments
@@ -43,19 +46,66 @@ fn main() {
     let source_encoding = args.value_of("source-encoding").unwrap_or("utf-8");
     let target_encoding = args.value_of("target-encoding").unwrap_or("utf-8");
     let delimiter = args.value_of("delimiter").unwrap_or(",");
-    if let Some(input) = args.value_of("input") {
-        if let Some(output) = args.value_of("output") {
-            println!("{}", source_encoding);
-            println!("{}", target_encoding);
-            println!("{}", delimiter);
-            println!("{}", input);
-            println!("{}", output);
-        }
-        else {
-            println!("gonna eat you");
+    
+    if args.is_present("list-encodings") {
+        println!("Valid BadSV encodings:");
+        for encoding in VALID_ENCODINGS.iter() {
+            println!("* {}", encoding);
         }
     }
+    else if args.is_present("convert") {
+        if let Some((input, output)) = get_io(&args) {
+            if let Some(del) = validate_delimiter(delimiter) {
+                let file = match File::open(input) {
+                    Ok(f) => f,
+                    Err(_) => panic!("Error opening file.")
+                };
+                let data = dsv::parse(file, delimiter.as_bytes()[0]);
+            }
+            else {
+                panic!("Delimiter must be 1 byte in length")
+            }
+        }
+        else {
+            panic!("Both input and output files must be provided.");
+        }
+    }
+    else if args.is_present("regress") {
+        if let Some((input, output)) = get_io(&args) {
+            if let Some(del) = validate_delimiter(delimiter) {
+                let file = match File::open(input) {
+                    Ok(f) => f,
+                    Err(_) => panic!("Error opening file.")
+                };
+            }
+            else {
+                panic!("Delimiter must be 1 byte in length")
+            }
+        }
+        else {
+            panic!("Both input and output files must be provided.");
+        }
+    }
+    // Shouldn't get this far
+}
+
+/// Finds the input and output files from command-line matches, if found
+fn get_io(args: &clap::ArgMatches) -> Option<(String, String)>{
+    if let Some(input) = args.value_of("input") {
+        if let Some(output) = args.value_of("output") {
+            return Some((String::from(input), String::from(output)))
+        }
+    }
+    None
+}
+
+// Ensures the delimiter is one byte in length
+fn validate_delimiter(delimiter: &str) -> Option<u8> {
+    let bytes = delimiter.as_bytes();
+    if bytes.len() == 1 {
+        Some(bytes[0])
+    }
     else {
-        println!("gonna yeet you");
+        None
     }
 }
